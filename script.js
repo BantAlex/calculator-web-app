b3939b870e114626b5f687d7eff8ff7503e75582
Alex Bandis <bantalex3@gmail.com>
2024-05-12 23:36:34 +0300

    Add basic operator functions

---
+++ script.js
@@ -0,0 +1,20 @@
+//Addition
+function add(n1,n2){
+    return n1 + n2;
+}
+//Subtraction
+function subtraction(n1,n2){
+    return n1 - n2;
+}
+//Multiply
+function multiply(n1,n2){
+    return n1 * n2;
+}
+//Division
+function divide(n1,n2){
+    if (n2 === 0){
+        return 'ERROR: Division by 0 is not allowed by math gods'
+    } else {
+        return n1 / n2;
+    }
+}
