d7fa4461b758cf61753262936115fa09dc3b70cf
Alex Bandis <bantalex3@gmail.com>
2024-05-13 18:36:33 +0300

    Add another color scheme and operator buttons(js only)

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//Dom
+let display = document.querySelector('.display');
+let buttons = document.querySelector('.calc-buttons');
+let addButton = document.createElement('button');
+let subtractButton = document.createElement('button');
+let divideButton = document.createElement('button');
+let multiplyButton = document.createElement('button');
+let equalsButton = document.createElement('button');
+let clearButton = document.createElement('button');
+
+// let allOperators = [addButton,subtractButton,divideButton,multiplyButton,equalsButton,clearButton];
+// allOperators.forEach(button =>{
+//     button.classList.add(".calc-buttons");
+//     buttons.appendChild(button);
+// }); Add after number button
+
+
 //Addition
 function add(n1,n2){
     return n1 + n2;
@@ -13,8 +30,12 @@ function multiply(n1,n2){
 //Division
 function divide(n1,n2){
     if (n2 === 0){
-        return 'ERROR: Division by 0 is not allowed by math gods'
+        return 'ERROR: Division by 0 is not allowed by math gods'; //Remember to reduce text size to fill display!
     } else {
         return n1 / n2;
     }
+}
+//Operation 
+function operator(n1,op,n2){
+    return op(n1,n2);
 }
