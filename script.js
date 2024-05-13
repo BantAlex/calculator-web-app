d07529431b4658a773750b2c7f30f52068319e6c
Alex Bandis <bantalex3@gmail.com>
2024-05-13 21:47:12 +0300

    Add all buttons

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //Dom
 let display = document.querySelector('.display');
 let buttons = document.querySelector('.calc-buttons');
+let row0 = document.querySelector('.row0')
+let row1 = document.querySelector('.row1')
+let row2 = document.querySelector('.row2')
+let row3 = document.querySelector('.row3')
 let addButton = document.createElement('button');
 let subtractButton = document.createElement('button');
 let divideButton = document.createElement('button');
@@ -14,15 +18,56 @@ let clearButton = document.createElement('button');
 //     buttons.appendChild(button);
 // }); Add after number button
 
-//Numbered Buttons
+//All Buttons
 function generateNumbers(){
-    for (i=0; i<10; i++){
-        let number = document.createElement('button');
-        number.classList.add('numbers','num' + i);
-        buttons.appendChild(number);
-        number.textContent = i;
+    //Row 0
+    let number0 = document.createElement('button');
+        number0.classList.add('numbers','num0');
+        number0.textContent = 0;
+        row0.appendChild(number0);
+    let equals = document.createElement('button');
+        equals.classList.add('numbers','equals');
+        equals.textContent = '=';
+        row0.appendChild(equals);
+    let clear = document.createElement('button');
+        clear.classList.add('numbers','clear');
+        clear.textContent = 'C/A';
+        row0.appendChild(clear);
+        //Row 1
+    for (let i = 1; i<=3; i++){
+        let numRow1 = document.createElement('button');
+        numRow1.classList.add('numbers', 'num' + i);
+        numRow1.textContent = i;
+        row1.appendChild(numRow1);
     }
+    let minus = document.createElement('button');
+        minus.classList.add('numbers','minus');
+        minus.textContent = '-';
+        row1.appendChild(minus);
+    //Row 2
+    for (let i = 4; i<=6; i++){
+        let numRow2 = document.createElement('button');
+        numRow2.classList.add('numbers', 'num' + i);
+        numRow2.textContent = i;
+        row2.appendChild(numRow2);
+    }
+    let multiply = document.createElement('button');
+        multiply.classList.add('numbers', 'multiply');
+        multiply.textContent = 'X';
+        row2.appendChild(multiply);
+    //Row 3
+    for (let i = 7; i<=9; i++){
+        let numRow3 = document.createElement('button');
+        numRow3.classList.add('numbers', 'num' + i);
+        numRow3.textContent = i;
+        row3.appendChild(numRow3);
+    }
+    let division = document.createElement('button');
+        division.classList.add('numbers', 'division');
+        division.textContent = '÷';
+        row3.appendChild(division);
 }
+
 generateNumbers();
 //Addition
 function add(n1,n2){
